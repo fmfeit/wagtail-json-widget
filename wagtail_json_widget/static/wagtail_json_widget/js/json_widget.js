0bd50f53d3c7a27c14ff6f26c8aa498e6fccb028
fmfeit <81036455+fmfeit@users.noreply.github.com>
2024-01-24 22:00:35 +0100

    Enable ad-hoc creation of new elements in InlinePanel.

--- wagtail_json_widget/static/wagtail_json_widget/js/json_widget.js
+++ wagtail_json_widget/static/wagtail_json_widget/js/json_widget.js
@@ -1,40 +1,51 @@
 class JSONEditorWidget {
+  constructor(element) {
+    this.initEditor(element);
+  }
 
-    constructor(element) {
-        this.initEditor(element);
-    }
+  initEditor(element) {
+    // Initialize the JSONEditor widget
+    const textarea = element.querySelector("textarea");
+    const options = {
+      modes: ["text", "code", "tree", "form", "view"],
+      mode: "tree",
+      search: false,
+    };
 
-    initEditor(element) {
-        // Initialize the JSONEditor widget
-        const textarea = element.querySelector('textarea');
-        const options = {"modes": ["text", "code", "tree", "form", "view"], "mode": "tree", "search": false};
-
-        options.onChange = function () {
-            var json = editor.get();
-            textarea.value=JSON.stringify(json);
-        }
-        const editor = new JSONEditor(element, options);
-        editor.set(JSON.parse(textarea.value));
-    }
+    options.onChange = function () {
+      var json = editor.get();
+      textarea.value = JSON.stringify(json);
+    };
+    const editor = new JSONEditor(element, options);
+    editor.set(JSON.parse(textarea.value));
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    createJSONFieldEditors();
+document.addEventListener("w-formset:ready", function () {
+  createJSONFieldEditors();
+});
+
+document.addEventListener("w-formset:added", function (event) {
+  addJSONFieldEditor(event.target);
 });
 
 function createJSONFieldEditors() {
-
-    const els = document.getElementsByClassName('w-field--json_editor_widget');
-    for (const el of els) {
-        const collection = el.getElementsByClassName('w-field__input')
-        if (collection.length === 0) {
-            continue;
-        }
-
-        // So we don't duplicate editor widgets if we have JSONBlock in streamfields
-        // and model field elements on the same page.
-        if (collection[0].firstElementChild.nodeName.toUpperCase() == 'TEXTAREA') {
-            new JSONEditorWidget(collection[0]);
-        }
+  const els = document.getElementsByClassName("w-field--json_editor_widget");
+  for (const el of els) {
+    const collection = el.getElementsByClassName("w-field__input");
+    if (collection.length === 0) {
+      continue;
     }
+
+    // So we don't duplicate editor widgets if we have JSONBlock in streamfields
+    // and model field elements on the same page.
+    if (collection[0].firstElementChild.nodeName.toUpperCase() == "TEXTAREA")
+      new JSONEditorWidget(collection[0]);
+  }
+}
+
+function addJSONFieldEditor(targetElement) {
+  const els = targetElement.getElementsByClassName("w-field__input");
+  if (els && els[0].firstElementChild.nodeName.toUpperCase() == "TEXTAREA")
+    new JSONEditorWidget(els[0]);
 }
